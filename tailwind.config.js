c0907b12557b9770bb1df1e07b018a2ff08e58f1
Sky DeGrate <skydegrate@gmail.com>
2023-04-01 12:33:42 -0400

    update: tried to install tailwind css framework

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html', 'node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js'],
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: ["lofi"]
-  }
-}
